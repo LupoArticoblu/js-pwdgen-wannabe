37a65751ae96d5cc6ab9bc2915c01bf1f9d6d799
LupoArticoblu <wolf94-@libero.it>
2022-10-10 17:31:35 +0200

    fine

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
-const nomeUtente = "Beppe"
-const cognomeUtente ="Junior"
+// const nomeUtente = "Beppe"
+// const cognomeUtente ="Junior"
 
-document.getElementById('password').innerHTML += nomeUtente + cognomeUtente;
+// document.getElementById('password').innerHTML += nomeUtente + cognomeUtente;
 
+// document.getElementById('password').innerHTML +=`
+//   <strong> ${nomeUtente} </strong> ${cognomeUtente}
+// `;
+
+const nomeUtente = prompt('Come ti chiami?');
+
+const cognomeUtente = prompt('Il cognome oooohhhh!!');
+
+const colorePrefe = prompt('Scegli un colore');
 
 document.getElementById('password').innerHTML +=`
-  <strong> ${nomeUtente} </strong> ${cognomeUtente}
+  ${nomeUtente} ${cognomeUtente} ${colorePrefe} 22
 `;
